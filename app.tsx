95ebcaa04e8d8052fac43115be28f9c0794d6d02
AkashGillNiet <24862486gill@gmail.com>
2025-01-05 09:53:02 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextToSpeech from './components/TextToSpeech';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TextToSpeech />
+    </div>
+  );
+}
+
+export default App;
